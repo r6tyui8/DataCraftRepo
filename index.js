4eb50d40931fe0f7c4a262eda48a394cd46da496
r6tyui8 <gs1duneidan@hotmail.com>
2024-05-12 18:12:10 +0700

    feat

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-function minDepth(root) {
-  if (!root) return 0;
-  if (!root.left && !root.right) return 1;
-  let min = Infinity;
-  if (root.left) min = Math.min(min, minDepth(root.left));
-  if (root.right) min = Math.min(min, minDepth(root.right));
-  return min + 1;
-}
+const numbers = [1, 5, 2, 8];
+const max = Math.max(...numbers);
+console.log(max); // 8
